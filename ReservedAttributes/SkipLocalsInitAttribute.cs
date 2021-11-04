3d7854f31aa39a544ca1af7aaffff787c554a586
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-11-04 11:22:54 +0900

    Add contents

---
+++ ReservedAttributes/SkipLocalsInitAttribute.cs
@@ -0,0 +1,6 @@
+﻿
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Module | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false)]
+    internal sealed class SkipLocalsInitAttribute : Attribute { }
+}
