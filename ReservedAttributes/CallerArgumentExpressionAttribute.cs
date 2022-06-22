ce8a1f5c3bf57730cfc4eb68b9d9a10f82bd8846
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-06-22 17:09:15 +0900

    CallerArgumentExpressionAttribute

---
+++ ReservedAttributes/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,11 @@
+﻿#if !NETCOREAPP3_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public string ParameterName { get; }
+        public CallerArgumentExpressionAttribute(string parameterName) => ParameterName = parameterName;
+    }
+}
+#endif
