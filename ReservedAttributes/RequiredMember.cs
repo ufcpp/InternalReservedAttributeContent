fe8e229a0d0ad55ee460efd8efe51fcc3648401c
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-06-22 17:16:19 +0900

    RequiredMemberAttribute

---
+++ ReservedAttributes/RequiredMember.cs
@@ -0,0 +1,21 @@
+#if !NET7_0_OR_GREATER
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    internal sealed class SetsRequiredMembersAttribute : Attribute { }
+}
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute
+    {
+        public RequiredMemberAttribute() { }
+    }
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public string FeatureName { get; }
+        public CompilerFeatureRequiredAttribute(string featureName) => FeatureName = featureName;
+    }
+}
+#endif
