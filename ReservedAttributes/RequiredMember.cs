5f8f933f8e25285ab4ea8a2b47b185a5b2b18db7
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-11-09 11:12:51 +0900

    [fix] remove duplicated attribute

--- ReservedAttributes/RequiredMember.cs
+++ ReservedAttributes/RequiredMember.cs
@@ -8,11 +8,5 @@ namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     internal sealed class RequiredMemberAttribute : Attribute { }
-    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-    internal sealed class CompilerFeatureRequiredAttribute : Attribute
-    {
-        public string FeatureName { get; }
-        public CompilerFeatureRequiredAttribute(string featureName) => FeatureName = featureName;
-    }
 }
 #endif
