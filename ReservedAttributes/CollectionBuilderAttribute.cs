285a12dfa5e33d2258df7b4d6fb0d47d8fbf9a66
Nobuyuki Iwanaga <ufcpp@live.jp>
2026-05-29 15:24:03 +0900

    CollectionBuilder and OverloadResolutionPriority

---
+++ ReservedAttributes/CollectionBuilderAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Runtime.CompilerServices
+{
+#if !NET8_0_OR_GREATER
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct, Inherited = false)]
+    internal sealed class CollectionBuilderAttribute(Type builderType, string methodName) : Attribute
+    {
+        public Type BuilderType { get; } = builderType;
+        public string MethodName { get; } = methodName;
+    }
+#endif
+}
---
+++ ReservedAttributes/OverloadResolutionPriorityAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Runtime.CompilerServices
+{
+#if !NET9_0_OR_GREATER
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class OverloadResolutionPriorityAttribute(int priority) : Attribute
+    {
+        public int Priority { get; } = priority;
+    }
+#endif
+}
